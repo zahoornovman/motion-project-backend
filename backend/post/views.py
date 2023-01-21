417a3fa695022a40e8b112f71cad8c14100df876
Martin P <45636696+martinpoehl@users.noreply.github.com>
2023-01-22 00:44:45 +0100

    Added Post API with filter-functionality

--- backend/post/views.py
+++ backend/post/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import filters
+
+from post.models import Post
+from post.serializers import PostSerializer
+
 
 # Create your views here.
+
+class ListCreatePostView(ListCreateAPIView):
+    queryset = Post.objects.all().order_by('created_time').reverse()
+    serializer_class = PostSerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['content']
+
+class RetrieveUpdateDeletePostView(RetrieveUpdateDestroyAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+class SearchPostView(generics.ListCreateAPIView):
+    queryset = Post.objects.all().order_by('created_time').reverse()
+    serializer_class = PostSerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['content']
